98701f2320216543f00f3190dbff3dba6da00a84
Mohammed Siddick.E <siddick007@yahoo.co.in>
2009-12-17 16:46:48 +0530

    Adding Array_Form Component

---
+++ test/ft_test5.js
@@ -0,0 +1,57 @@
+var NameListFormClass = FT.Component.Form.create({
+	fields: {
+		names: FT.Component.ArrayForm.create({
+			form: FT.Component.Form.create({
+				fields: {
+					name: FT.Component.InputBox,
+					close: FT.Component.Button.create({
+						events: {
+							click: function( eventObj ){
+								var form = this.getParent();
+								form.getParent().removeForm( form );
+								Event.stop( eventObj );
+							}
+						}
+					}),
+					up: FT.Component.Button.create({
+						events: {
+							click: function( eventObj ){
+								var form = this.getParent();
+								form.getParent().upForm( form );
+								Event.stop( eventObj );
+							}
+						}
+					}),
+					down: FT.Component.Button.create({
+						events: {
+							click: function( eventObj ){
+								var form = this.getParent();
+								form.getParent().downForm( form );
+								Event.stop( eventObj );
+							}
+						}
+					})
+				}
+			})
+		}),
+		add: FT.Component.Button.create({
+			events: {
+				click: function(){
+					this.getParent().getField('names').addForm();
+				}
+			}
+		}),
+		submit: FT.Component.Button.create({
+			events: {
+				click: function(){
+					alert(this.getParent().getValue().toJSON());
+				}
+			}
+		})
+	}
+});
+
+Event.observe( window, 'load', function(){
+	
+ 	var form = new NameListFormClass($('list'));
+});
